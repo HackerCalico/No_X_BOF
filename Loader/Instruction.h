4c48567e70a9864f55718973fdd22e0f015ca021
Calico <112098778+HackerCalico@users.noreply.github.com>
2024-07-08 12:14:24 +0800

    Delete Loader directory

--- Loader/Instruction.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-int InvokeInstruction(int mnemonicIndex, char opType1, char opBit1, DWORD_PTR opAddr1, char opBit2, DWORD_PTR opAddr2, PDWORD_PTR pVtRegs);
--- Loader/Interpreter.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void MagicInvoke(char* selfAsm, char* commandPara, int commandParaLength, char** pOutputData, int* pOutputDataLength, PVOID* pFuncAddr);
